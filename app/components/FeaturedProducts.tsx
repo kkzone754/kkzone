38249b716b36aeaa6cefe6411b8c136e33b7d350
K.K. ZONE <k.k.zone754@gmail.com>
2026-08-30 00:14:57 +0500

    fix: Add missing id prop to ProductCard in FeaturedProducts

--- app/components/FeaturedProducts.tsx
+++ app/components/FeaturedProducts.tsx
@@ -14,6 +14,7 @@ export default function FeaturedProducts() {
         {products.map((product) => (
           <ProductCard
             key={product.id}
+            id={product.id}
             name={product.name}
             price={product.price}
             image={product.image}
